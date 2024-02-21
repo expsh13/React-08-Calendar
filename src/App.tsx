cfc6ad1d11d29bc22010d9a532153f69201ff4f5
Satoki Hata <satokihata@gmail.com>
2024-02-21 13:15:10 +0900

    add stye

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { Calendar } from "./components/Calendar/Calendar";
+import { CalendarHeader } from "./components/Calendar/CalendarHeader";
+
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="w-11/12 m-auto">
+      <CalendarHeader />
+      <Calendar />
+    </div>
+  );
 };
---
+++ src/components/Calendar/CalendarHeader.tsx
@@ -0,0 +1,15 @@
+export const CalendarHeader = () => {
+  return (
+    <div className="flex gap-4">
+      <h1 className="text-2xl font-bold">カレンダー</h1>
+      <button className="border p-1 border-gray-400 bg-white rounded">
+        今日
+      </button>
+      <div>
+        <button>＜</button>
+        <button>＞</button>
+      </div>
+      <p>2024年2月</p>
+    </div>
+  );
+};
