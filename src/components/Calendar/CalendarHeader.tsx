799c6c1f946f87bd5a452bf33ea0b7653bcc25f8
Satoki Hata <satokihata@gmail.com>
2024-02-25 23:19:00 +0900

    update style

--- src/components/Calendar/CalendarHeader.tsx
+++ src/components/Calendar/CalendarHeader.tsx
@@ -1,6 +1,6 @@
 export const CalendarHeader = () => {
   return (
-    <div className="flex gap-4">
+    <div className="flex gap-4 items-center">
       <h1 className="text-2xl font-bold">カレンダー</h1>
       <button className="border p-1 border-gray-400 bg-white rounded">
         今日
