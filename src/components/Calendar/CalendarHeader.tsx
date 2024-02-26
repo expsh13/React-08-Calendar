d197a294cedb11c06e0ef871b94ac0129b352954
Satoki Hata <satokihata@gmail.com>
2024-02-26 19:50:13 +0900

    update style

--- src/components/Calendar/CalendarHeader.tsx
+++ src/components/Calendar/CalendarHeader.tsx
@@ -18,7 +18,7 @@ export const CalendarHeader = (props: PropsType) => {
   };
 
   return (
-    <div className="flex gap-4 items-center mt-3 mb-3">
+    <div className="flex gap-6 items-center mt-3 mb-3">
       <h1 className="text-2xl font-bold">カレンダー</h1>
       <button
         className="border p-1 border-gray-400 bg-white rounded"
@@ -26,9 +26,13 @@ export const CalendarHeader = (props: PropsType) => {
       >
         今日
       </button>
-      <div>
-        <button onClick={handleSubtractMonth}>＜</button>
-        <button onClick={handleAddMonth}>＞</button>
+      <div className="flex gap-1">
+        <button className="hover:cursor-pointer" onClick={handleSubtractMonth}>
+          ＜
+        </button>
+        <button className="hover:cursor-pointer" onClick={handleAddMonth}>
+          ＞
+        </button>
       </div>
       <p>
         {month.getFullYear()}年{month.getMonth() + 1}月
