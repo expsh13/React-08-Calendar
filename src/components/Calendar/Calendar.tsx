a047139cd7d7b048f771f39d7755a1bbb68333ad
Satoki Hata <satokihata@gmail.com>
2024-03-01 23:59:57 +0900

    add test

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -2,32 +2,6 @@ type PropsType = {
   currentDate: Date;
 };
 
-const weeks = ["日", "月", "火", "水", "木", "金", "土"];
-
-const currentDateRange = (currentDate: Date): Date[] => {
-  const currentMonthStartDate = new Date(currentDate); // 月の最初の日
-  currentMonthStartDate.setDate(1);
-  const currentMonthEndDate = new Date(currentDate); // 月の最終日
-  currentMonthEndDate.setMonth(currentDate.getMonth() + 1);
-  currentMonthEndDate.setDate(0);
-
-  const currentMonthStartDay = currentMonthStartDate.getDay(); // 月の最初の日の曜日を取得
-  const currentMonthEndDay = currentMonthEndDate.getDay(); // 月の最終日の曜日
-
-  const lastMonthRange = weeks.length - (weeks.length - currentMonthStartDay);
-  const currentMonthRange = Math.floor(
-    (currentMonthEndDate.getTime() - currentMonthStartDate.getTime()) /
-      (1000 * 60 * 60 * 24)
-  );
-  const nextMonthRange = weeks.length - currentMonthEndDay;
-  const dateArray: Date[] = [];
-  for (let i = 1; i < lastMonthRange + 1; i++) {
-    const newDate = new Date(currentMonthStartDate);
-    newDate.setDate(newDate.getDate() - (lastMonthRange - i));
-    dateArray.push();
-  }
-};
-
 export const Calendar = (props: PropsType) => {
   const { currentDate } = props;
 
